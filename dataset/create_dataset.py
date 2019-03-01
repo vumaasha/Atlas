216a88ac0e1761055588fdc027fa0ed215255d28
GirishShanmugam <s.girishshanmugam@gmail.com>
2019-03-01 13:05:56 +0530

    Save images in zvsn ipynb

--- dataset/create_dataset.py
+++ dataset/create_dataset.py
@@ -1,6 +1,7 @@
 import argparse
 import urllib.request
 import json
+from tqdm import tqdm
 import os
 cwd = os.getcwd()
 
@@ -10,17 +11,17 @@ def create_atlas_dataset():
 
 
 def create_zvsn_dataset():
-    if not os.path.exists("Normal_vs_Zoomed/Normal"):
-        os.makedirs("normal")
-    if not os.path.exists("Normal_vs_Zoomed/Zoomed"):
-        os.makedirs("zoomed")
+    if not os.path.exists("Normal_vs_Zoomed/normal"):
+        os.makedirs("Normal_vs_Zoomed/normal")
+    if not os.path.exists("Normal_vs_Zoomed/zoomed"):
+        os.makedirs("Normal_vs_Zoomed/zoomed")
 
     # load list of images from JSON
-    with open('zvsn_data.json') as data_file:
+    with open(cwd+'/zvsn_data.json') as data_file:
         images_data = json.load(data_file)
 
     # download image from URL and save it in corresponding folders
-    for img in images_data:
+    for img in tqdm(images_data):
         location = cwd + "/Normal_vs_Zoomed/" +img['label']+ "/" + img['filename']
         urllib.request.urlretrieve(img['image_url'], location)
 
