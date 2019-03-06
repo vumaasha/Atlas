96db890d98d45a6e1c7ca0e4ac91ad56d000fde7
Aditi <aditiap2205@gmail.com>
2019-03-06 19:24:21 +0530

    tested mirraw_crawler.py

--- data_collection/Crawler/spiders/mirraw_crawler.py
+++ data_collection/Crawler/spiders/mirraw_crawler.py
@@ -10,20 +10,21 @@ from ..Utils import write_into_json
 class MirrawFashion(scrapy.Spider):
     name = "mirraw_crawler"
     custom_settings = {
+        'IMAGES_STORE': '/home/et/Desktop/Atlas/dataset/',
         'ITEM_PIPELINES': {
             'Crawler.pipelines.MirrawPipeline': 1
         }
     }
 
-    input_csv_file = 'Sheet11.csv'  # csv file containing the taxonomy and website source URL's
     source_urls_col = 'Mirraw'  # Column name having the source URL's in CSV file
     taxonomy_col = 'Taxonomy'  # Column name having the taxonomy of the product
 
-    map_file = pd.read_csv(input_csv_file)
 
     def start_requests(self):
+        input_csv_file = '/home/et/Desktop/Atlas/data_collection/dataset.csv'  # csv file containing the taxonomy and website source URL's
+        map_file = pd.read_csv(input_csv_file)
         start_request_list = []
-        for index, row in self.map_file.dropna(subset=[self.source_urls_col]).iterrows():
+        for index, row in map_file.dropna(subset=[self.source_urls_col]).iterrows():
             taxonomy = row[self.taxonomy_col]
             source_url = row[self.source_urls_col]
             start_request_list.append(scrapy.Request(source_url, callback=self.parse, meta={'taxonomy': taxonomy}))
@@ -104,13 +105,13 @@ class MirrawFashion(scrapy.Spider):
                 dict_of_items[list_of_specs[i]] = list_of_specs[i + 1]
 
         image_file_name = product_image_url.split('/')[-1]
-        file_path = response.meta['taxonomy'].replace("->",
-                                                      "/") + "/" + self.source_urls_col + "/images/" + image_file_name
+        temp_taxonomy = response.meta['taxonomy'].replace(" ", "_")
+        file_path = 'atlas_dataset/' + temp_taxonomy.replace("->",
+                                                             "-") + "/images/" + image_file_name
         dict_of_items['taxonomy'] = response.meta['taxonomy']
         dict_of_items['file_path'] = file_path
-        json_path = 'images/' + response.meta['taxonomy'].replace("->",
-                                                                  "/") + "/" + self.source_urls_col + '/'
-
+        json_path = '/home/et/Desktop/Atlas/dataset/atlas_dataset/' + temp_taxonomy.replace("->",
+                                                                                            "-") + "/"
         write_into_json(json_path,dict_of_items)
 
         yield MirrawItem(image_url=product_image_url, image_name=image_file_name, image_path=file_path)
