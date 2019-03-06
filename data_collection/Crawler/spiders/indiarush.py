76032f1768e959e29a56da958709bc873946304f
Aditi <aditiap2205@gmail.com>
2019-03-06 18:33:49 +0530

    tested indiarush.py

--- data_collection/Crawler/spiders/indiarush.py
+++ data_collection/Crawler/spiders/indiarush.py
@@ -9,20 +9,23 @@ from ..items import IndiaRushItem
 class IndiaRush(scrapy.Spider):
     name = "indiarush_crawler"
     custom_settings = {
+        'IMAGES_STORE': '/home/et/Desktop/Atlas/dataset/',
         'ITEM_PIPELINES': {
             'Crawler.pipelines.IndiaRushPipeline': 1
         }
     }
 
-    input_csv_file = 'Sheet11.csv'  # csv file containing the taxonomy and website source URL's
+
     source_urls_col = 'IndiaRush'  # Column name having the source URL's in CSV file
     taxonomy_col = 'Taxonomy'  # Column name having the taxonomy of the product
 
-    map_file = pd.read_csv(input_csv_file)
+
 
     def start_requests(self):
+        input_csv_file = '/home/et/Desktop/Atlas/data_collection/dataset.csv'  # csv file containing the taxonomy and website source URL's
+        map_file = pd.read_csv(input_csv_file)
         start_request_list = []
-        for index, row in self.map_file.dropna(subset=[self.source_urls_col]).iterrows():
+        for index, row in map_file.dropna(subset=[self.source_urls_col]).iterrows():
             taxonomy = row[self.taxonomy_col]
             source_url = row[self.source_urls_col]
             start_request_list.append(scrapy.Request(source_url, callback=self.parse, meta={'taxonomy': taxonomy}))
@@ -52,13 +55,14 @@ class IndiaRush(scrapy.Spider):
         for i in range(0, len(list_of_specs) - 1, 2):
             dict_of_items[re.sub(':', ' ', list_of_specs[i])] = list_of_specs[i+1]
         image_file_name = product_image_url.split('/')[-1]
-        file_path = response.meta['taxonomy'].replace("->",
-                                                      "/") + "/" + self.source_urls_col + "/images/" + image_file_name
+        temp_taxonomy = response.meta['taxonomy'].replace(" ","_")
+        file_path = 'atlas_dataset/'+temp_taxonomy.replace("->",
+                                                      "-") + "/images/" + image_file_name
         dict_of_items['file_path'] = file_path
         dict_of_items['taxonomy'] = response.meta['taxonomy']
 
-        json_path = 'images/' + response.meta['taxonomy'].replace("->",
-                                                                  "/") + "/" + self.source_urls_col + '/'
+        json_path = '/home/et/Desktop/Atlas/dataset/atlas_dataset/'+temp_taxonomy.replace("->",
+                                                      "-") + "/"
         write_into_json(json_path,dict_of_items)
 
         yield IndiaRushItem(image_url=product_image_url, image_name=image_file_name, image_path=file_path)
