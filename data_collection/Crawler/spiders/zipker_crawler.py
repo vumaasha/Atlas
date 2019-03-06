4b14dafed17bb31acf686cb255fb59033c1269d9
Aditi <aditiap2205@gmail.com>
2019-03-06 19:38:13 +0530

    tested zipker_crawler.py

--- data_collection/Crawler/spiders/zipker_crawler.py
+++ data_collection/Crawler/spiders/zipker_crawler.py
@@ -10,21 +10,20 @@ from ..items import ZipkerItem
 class Zipker(scrapy.Spider):
     name = "zipker_crawler"
     custom_settings = {
+        'IMAGES_STORE': '/home/et/Desktop/Atlas/dataset/',
         'ITEM_PIPELINES': {
             'Crawler.pipelines.ZipkerPipeline': 1
         }
     }
 
-    input_csv_file = 'IndianClothesWomen.csv'  # csv file containing the taxonomy and website source URL's
     source_urls_col = 'Zipker'  # Column name having the source URL's in CSV file
     taxonomy_col = 'Taxonomy'  # Column name having the taxonomy of the product
 
-    map_file = pd.read_csv(input_csv_file)
-
-
     def start_requests(self):
+        input_csv_file = '/home/et/Desktop/Atlas/data_collection/dataset.csv'  # csv file containing the taxonomy and website source URL's
+        map_file = pd.read_csv(input_csv_file)
         start_request_list = []
-        for index, row in self.map_file.dropna(subset=[self.source_urls_col]).iterrows():
+        for index, row in map_file.dropna(subset=[self.source_urls_col]).iterrows():
             taxonomy = row[self.taxonomy_col]
             source_url = row[self.source_urls_col]
             start_request_list.append(scrapy.Request(source_url, callback=self.parse, meta={'taxonomy': taxonomy}))
@@ -60,12 +59,13 @@ class Zipker(scrapy.Spider):
         dict_of_items.pop('Note',None)
 
         image_file_name = product_image_url.split('/')[-1]
-        file_path = response.meta['taxonomy'].replace("->",
-                                                      "/") + "/" + self.source_urls_col + "/images/" + image_file_name
+        temp_taxonomy = response.meta['taxonomy'].replace(" ", "_")
+        file_path = 'atlas_dataset/' + temp_taxonomy.replace("->",
+                                                             "-") + "/images/" + image_file_name
         dict_of_items['taxonomy'] = response.meta['taxonomy']
         dict_of_items['file_path'] = file_path
-        json_path = 'images/' + response.meta['taxonomy'].replace("->",
-                                                                  "/") + "/" + self.source_urls_col + '/'
+        json_path = '/home/et/Desktop/Atlas/dataset/atlas_dataset/' + temp_taxonomy.replace("->",
+                                                                                            "-") + "/"
         write_into_json(json_path,dict_of_items)
 
         yield ZipkerItem(image_url=product_image_url, image_name=image_file_name, image_path=file_path)
