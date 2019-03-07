0890008b93d64c7d41ca594d65e754628574444e
Aditi <aditiap2205@gmail.com>
2019-03-07 21:46:20 +0530

    modified generate_split file

--- data_collection/generate_split_json.py
+++ data_collection/generate_split_json.py
@@ -2,10 +2,11 @@ import glob
 import json
 import os
 import random
+import argparse
 
-
-def get_product_data_with_split(categories, train_split_val=0.70, val_split_val=0.20):
+def get_product_data_with_split(source_folder,categories, train_split_val=0.70, val_split_val=0.20):
     full_product_data_list = []
+    temp = source_folder.split('/atlas_dataset')[0]
     for category in categories:
         # Get list of images in source folder
         images_list = glob.glob(source_folder + category + '/images/*.jp*', recursive=True)
@@ -18,6 +19,9 @@ def get_product_data_with_split(categories, train_split_val=0.70, val_split_val=
             actual_data = {'filename': product_data['file_path'], 'title': product_data['product_title'],
                            'sentences': [{'tokens': product_data['taxonomy'].split('/')}]}
             cleaned_json_data.append(actual_data)
+
+        for i in cleaned_json_data:
+            i['filename'] = temp+'/'+i['filename']
 
         # Get product data for the images
         product_data = list(filter(lambda x: x['filename'] in images_list, cleaned_json_data))
@@ -43,17 +47,24 @@ def get_product_data_with_split(categories, train_split_val=0.70, val_split_val=
     return full_product_data_list
 
 
-source_folder = '/home/shinchan/atlas_datasets/coresdataset19/'
 
-# get categories/labels list
-categories = os.listdir(source_folder)
-print('{} Categories have been found: {}'.format(len(categories), categories))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script which splits the data into Train, Validate and Test to train the product_categorization model')
+    parser.add_argument('--dataset_path', '-dp', help='Path to atlas_dataset')
 
-# get full product data along with split
-product_data_with_split = get_product_data_with_split(categories)
-print('Total number of samples:{}'.format(len(product_data_with_split)))
+    args = parser.parse_args()
+    source_folder = args.dataset_path
+    json_source_folder = source_folder.split('atlas_dataset')[0]
 
-# save it as a json file
-json_file_name = 'coresdataset19'
-with open(source_folder+json_file_name+'.json', 'w') as outfile:
-    json.dump({'images': product_data_with_split}, outfile)
+    # get categories/labels list
+    categories = os.listdir(source_folder)
+    print('{} Categories have been found: {}'.format(len(categories), categories))
+
+    # get full product data along with split
+    product_data_with_split = get_product_data_with_split(source_folder,categories)
+    print('Total number of samples:{}'.format(len(product_data_with_split)))
+
+    # save it as a json file
+    json_file_name = 'atlas_dataset'
+    with open(json_source_folder+json_file_name+'.json', 'w') as outfile:
+        json.dump({'images': product_data_with_split}, outfile)
