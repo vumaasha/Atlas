df999edb39fd2830e5a1bbd68998935ff0087741
Aditi <aditiap2205@gmail.com>
2019-03-11 14:16:02 +0530

    Completed and Tested

--- data_collection/generate_split_json.py
+++ data_collection/generate_split_json.py
@@ -4,13 +4,13 @@ import os
 import random
 import argparse
 
+
 def get_product_data_with_split(source_folder,categories, train_split_val=0.70, val_split_val=0.20):
     full_product_data_list = []
-    temp = source_folder.split('/atlas_dataset')[0]
     for category in categories:
         # Get list of images in source folder
-        images_list = glob.glob(source_folder + category + '/images/*.jp*', recursive=True)
-        with open(source_folder + category + '/data.json', 'r') as f:
+        images_list = glob.glob(source_folder + 'atlas_dataset/' + category + '/images/*.jp*', recursive=True)
+        with open(source_folder + 'atlas_dataset/' + category + '/data.json', 'r') as f:
             json_data = json.load(f)
 
         cleaned_json_data = []
@@ -20,11 +20,8 @@ def get_product_data_with_split(source_folder,categories, train_split_val=0.70, 
                            'sentences': [{'tokens': product_data['taxonomy'].split('/')}]}
             cleaned_json_data.append(actual_data)
 
-        for i in cleaned_json_data:
-            i['filename'] = temp+'/'+i['filename']
-
         # Get product data for the images
-        product_data = list(filter(lambda x: x['filename'] in images_list, cleaned_json_data))
+        product_data = list(filter(lambda x: source_folder + x['filename'] in images_list, cleaned_json_data))
         print('Total number of products in {}: {}'.format(category, len(product_data)))
 
         # shuffle the list
@@ -47,17 +44,17 @@ def get_product_data_with_split(source_folder,categories, train_split_val=0.70, 
     return full_product_data_list
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script which splits the data into Train, Validate and Test to train the product_categorization model')
     parser.add_argument('--dataset_path', '-dp', help='Path to atlas_dataset')
-
+    parser.add_argument('--destination_path', '-dp', help='Destination path to atlas_dataset.json file')
     args = parser.parse_args()
+
     source_folder = args.dataset_path
-    json_source_folder = source_folder.split('atlas_dataset')[0]
+    dest_folder = args.destination_path
 
     # get categories/labels list
-    categories = os.listdir(source_folder)
+    categories = os.listdir(source_folder+'/atlas_dataset')
     print('{} Categories have been found: {}'.format(len(categories), categories))
 
     # get full product data along with split
@@ -65,6 +62,7 @@ if __name__ == '__main__':
     print('Total number of samples:{}'.format(len(product_data_with_split)))
 
     # save it as a json file
-    json_file_name = 'atlas_dataset'
-    with open(json_source_folder+json_file_name+'.json', 'w') as outfile:
+    json_file_name = '/atlas_dataset'
+    with open(dest_folder+json_file_name+'.json', 'w') as outfile:
         json.dump({'images': product_data_with_split}, outfile)
+
