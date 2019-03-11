4db0dd97562c6de78b9663aa3eb17b67ad6ca458
Aditi <aditiap2205@gmail.com>
2019-03-11 14:12:05 +0530

    Completed and Tested

--- data_collection/Crawler/spiders/zipker_crawler.py
+++ data_collection/Crawler/spiders/zipker_crawler.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import requests
 import scrapy
-from tqdm import tqdm
 import re
 from ..Utils import write_into_json
 
@@ -10,18 +9,23 @@ from ..items import ZipkerItem
 class Zipker(scrapy.Spider):
     name = "zipker_crawler"
     custom_settings = {
-        'IMAGES_STORE': '/home/et/Desktop/Atlas/dataset/',
+        'IMAGES_STORE': '',
         'ITEM_PIPELINES': {
             'Crawler.pipelines.ZipkerPipeline': 1
         }
     }
 
-    source_urls_col = 'Zipker'  # Column name having the source URL's in CSV file
+    source_urls_col = 'zipker'  # Column name having the source URL's in CSV file
     taxonomy_col = 'Taxonomy'  # Column name having the taxonomy of the product
 
+
+    def __init__(self,input_csv_path="",*args, **kwargs):
+        super(Zipker, self).__init__(*args, **kwargs)
+        self.input_csv_path = input_csv_path
+
+
     def start_requests(self):
-        input_csv_file = '/home/et/Desktop/Atlas/data_collection/dataset.csv'  # csv file containing the taxonomy and website source URL's
-        map_file = pd.read_csv(input_csv_file)
+        map_file = pd.read_csv(self.input_csv_path)
         start_request_list = []
         for index, row in map_file.dropna(subset=[self.source_urls_col]).iterrows():
             taxonomy = row[self.taxonomy_col]
@@ -30,9 +34,10 @@ class Zipker(scrapy.Spider):
         return start_request_list
 
     def parse(self, response):
-        all_items = response.css('div#catalog-listing a.product-image::attr(href)').extract()
-        self.logger.info('All items for {} is {}'.format(response.meta['taxonomy'], len(all_items)))
-        for product_page_url in all_items:
+        # gets list of product page urls from source url specified
+        all_items_in_url = response.css('div#catalog-listing a.product-image::attr(href)').extract()
+        self.logger.info('All items for {} is {}'.format(response.meta['taxonomy'], len(all_items_in_url)))
+        for product_page_url in all_items_in_url:
             taxonomy = response.meta['taxonomy']
             yield scrapy.Request(product_page_url, callback=self.parse_product,
                                  meta={'product_page_url': product_page_url, 'taxonomy': taxonomy})
@@ -43,11 +48,13 @@ class Zipker(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse, meta={'taxonomy': taxonomy})
 
     def parse_product(self, response):
-        dict_of_items = {}
-        dict_of_items['product_title'] = response.css('div.product-mainp div.product-name.product_name_h1 span.h1::text').extract_first()
-        dict_of_items['product_price'] = re.sub("\s+"," ",response.css('div.product-mainp div.Product-price div.price-box span.price::text').extract_first())
+        # Parses each product page and retrieves product details
+        product_details = {}
+        product_details['product_title'] = response.css('div.product-mainp div.product-name.product_name_h1 span.h1::text').extract_first()
+        product_details['product_price'] = re.sub("\s+"," ",response.css('div.product-mainp div.Product-price div.price-box span.price::text').extract_first())
         product_image_url = response.css('div.product-mainp div.box1 img::attr(src)').extract_first()
-        dict_of_items['product_image_url'] = product_image_url
+        product_details['product_image_url'] = product_image_url
+        product_details['product_page_url'] = response.meta['product_page_url']
 
         product_specs = response.css('div.short-description.desc_pro div.std::text').extract()
         product_specs = [a for a in product_specs if a != '\n']
@@ -55,18 +62,20 @@ class Zipker(scrapy.Spider):
         for spec in product_specs:
             spec_split = spec.split(":")
             if len(spec_split) > 1:
-                dict_of_items[spec_split[0]] = spec_split[1]
-        dict_of_items.pop('Note',None)
+                product_details[spec_split[0]] = spec_split[1]
+        product_details.pop('Note',None)
 
         image_file_name = product_image_url.split('/')[-1]
         temp_taxonomy = response.meta['taxonomy'].replace(" ", "_")
         file_path = 'atlas_dataset/' + temp_taxonomy.replace("->",
                                                              "-") + "/images/" + image_file_name
-        dict_of_items['taxonomy'] = response.meta['taxonomy']
-        dict_of_items['file_path'] = file_path
+        product_details['taxonomy'] = response.meta['taxonomy']
+        product_details['file_path'] = file_path
+
+        # Writes product page details into json file called data.json
         json_path = '/home/et/Desktop/Atlas/dataset/atlas_dataset/' + temp_taxonomy.replace("->",
                                                                                             "-") + "/"
-        write_into_json(json_path,dict_of_items)
+        write_into_json(json_path,product_details)
 
         yield ZipkerItem(image_url=product_image_url, image_name=image_file_name, image_path=file_path)
 
